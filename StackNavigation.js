b83af093fa92d348531bb6464d9ebf869eada072
Sean Tolbert <sean@seans-mbp.mynetworksettings.com>
2023-10-20 10:08:18 -0400

    Starts Tab Navigator

--- StackNavigation.js
+++ StackNavigation.js
@@ -1,12 +1,21 @@
-import Feed from "./screens/Feed";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Feed from "./screens/Feed";
 
 const StackNavigation = () => {
+  const Tab = createBottomTabNavigator();
 
+  const TabGroup = () => {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen name="Feed" component={Feed} />
+      </Tab.Navigator>
+    );
+  };
 
   return (
     <NavigationContainer>
-      <Stack.Screen name="Feed" component={Feed} />
+      <TabGroup />
     </NavigationContainer>
   );
 };
